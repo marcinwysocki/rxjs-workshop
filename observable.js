bfd934a70e02b77a0553d841d1fa3c3d6b14233f
Marcin Wysocki <wysocki.marcin92@gmail.com>
2017-11-30 09:28:41 +0100

    stage1 solution

--- observable.js
+++ observable.js
@@ -9,12 +9,34 @@ class Observable {
 
     // ex. 1
     static of(...args) {
-        // your code...
+        return new Observable(observer => {
+            args.forEach(value => {
+                observer.next(value);
+            });
+
+            observer.complete();
+
+            return {
+                unsubscribe() {},
+            };
+        });
     }
 
     // ex. 2
     static interval(period) {
-        // your code...
+        return new Observable(observer => {
+            let counter = 0;
+            const interval = setInterval(() => {
+                observer.next(counter);
+                counter++;
+            }, period);
+
+            return {
+                unsubscribe() {
+                    clearInterval(interval);
+                },
+            };
+        });
     }
 }
 
